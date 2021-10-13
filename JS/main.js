2280e954b77850b40b166c13aed8b33cd8e4e9e5
yurioneix <yurioneix@gmail.com>
2021-10-13 16:36:55 -0300

    confirmar

---
+++ JS/main.js
@@ -0,0 +1,89 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar");   
+}
+
+/*function redirecionar(){
+    window.open("https://www.facebook.com");
+    window.location.href = "https://www.facebook.com";
+}
+*/
+
+function trocar(){
+    //alert("Trocar texto");
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar() {
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+/*
+function soma(n1,n2) {
+    return n1 + n2;
+}
+
+//var validar = 0;
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18) {
+        validar = true; 
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual é a sua idade?");
+console.log(validaIdade(idade));
+alert(soma(5, 10));
+*/
+
+
+/*
+var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+
+/*
+var count; 
+for (count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/* 
+var count = 0; 
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count ++;
+}
+*/
+
+/* 
+var idade = prompt("Qual é a sua idade"); 
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+    } 
+*/ 
+
+
+/*
+var nome = "Yuri Rodrigues";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2); 
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil")); */
+
